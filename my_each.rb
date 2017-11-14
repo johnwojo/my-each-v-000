3a5989355adc2a20c0f5580357506c9f761c2e00
John Wojciechowski <jwojo13@gmail.com>
2017-11-14 16:17:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
 array.collect do {|num|}
-  while num > array.length
-  array << num
-  array
-end
-end
+    while num > array.length
+      array << num
+      array
+    end
+  end
 end
